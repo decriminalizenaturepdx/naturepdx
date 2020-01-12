7a8b982537cc09defca266a02cbf7841014ccb38
moicez <moises.castaneda0@walmartlabs.com>
2020-01-12 14:42:56 -0800

    feature: add mission statement

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,11 +1,11 @@
 import { h } from 'preact'
 import style from './style'
 import SignUpForm from '../../components/sign-up-form'
+import MissionStatement from '../../components/mission-statement'
 
 const Home = () => (
   <div class={style.home}>
-    <h1>Home</h1>
-    <p>This is the Home component.</p>
+    <MissionStatement />
     <SignUpForm />
   </div>
 )
