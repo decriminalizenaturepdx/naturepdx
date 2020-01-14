02af0ea551485057863bb07a37fd25c25792c6c4
moicez <moises.castaneda0@walmartlabs.com>
2020-01-13 21:14:53 -0800

    Updates

---
+++ precache-manifest.0fd457ea35e3e5d4a06461b4530a86ec.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f49bd02b41f6387fafed",
+    "url": "/bundle.956a5.css"
+  },
+  {
+    "revision": "f49bd02b41f6387fafed",
+    "url": "/bundle.f49bd.js"
+  },
+  {
+    "revision": "8fd5fb542508cacbf6730c1163202689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95af2e800745efe41a3b",
+    "url": "/polyfills.95af2.js"
+  },
+  {
+    "revision": "a3eef53a411d79ccc662",
+    "url": "/route-home/index.jsx.chunk.470ef.css"
+  },
+  {
+    "revision": "a3eef53a411d79ccc662",
+    "url": "/route-home/index.jsx.chunk.a3eef.js"
+  },
+  {
+    "revision": "ad69f57f6efe404c34f5",
+    "url": "/route-profile/index.jsx.chunk.4025b.css"
+  },
+  {
+    "revision": "ad69f57f6efe404c34f5",
+    "url": "/route-profile/index.jsx.chunk.ad69f.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "f3e2cf9c1d4f8e7ad70a8079d758a80f",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  }
+]);
---
+++ precache-manifest.ee51968967b5767375de8ce09e5d5144.esm.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "101db1ad15378c13bb160f51b0e2e3e4",
+    "url": "/bundle.92f45.esm.js"
+  },
+  {
+    "revision": "f49bd02b41f6387fafed",
+    "url": "/bundle.956a5.css"
+  },
+  {
+    "revision": "8fd5fb542508cacbf6730c1163202689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b32712eff06f0c4b34c49f6c8e03e6d",
+    "url": "/polyfills.52c6c.esm.js"
+  },
+  {
+    "revision": "a3eef53a411d79ccc662",
+    "url": "/route-home/index.jsx.chunk.470ef.css"
+  },
+  {
+    "revision": "6809349d01f8753b2fd904baeae0cfa3",
+    "url": "/route-home/index.jsx.chunk.c6e56.esm.js"
+  },
+  {
+    "revision": "ad69f57f6efe404c34f5",
+    "url": "/route-profile/index.jsx.chunk.4025b.css"
+  },
+  {
+    "revision": "4c0e5adfdb026496d41c1d254df1faed",
+    "url": "/route-profile/index.jsx.chunk.46214.esm.js"
+  }
+]);
