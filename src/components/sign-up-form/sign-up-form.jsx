935676d0a3d18c8a966fcfb00e51fdece9ab5dfb
moicez <moises.castaneda0@walmartlabs.com>
2020-01-18 15:59:08 -0800

    refactor: drop bootstrap, introduce material ui

--- src/components/sign-up-form/sign-up-form.jsx
+++ src/components/sign-up-form/sign-up-form.jsx
@@ -1,33 +1,60 @@
 import { h } from 'preact'
 import { useState } from 'preact/hooks'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+  heading: {
+    margin: '32px 0 0 !important',
+  },
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+    '&': {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'baseline',
+    },
+    '& .Mui-error:after': {
+      borderBottomColor: '#9a1f16',
+    },
+    '& .Mui-error': {
+      color: '#9a1f16',
+    },
+  },
+}));
 
 const emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
 
 const SignUpForm = () => {
+  const classes = useStyles()
+  const [firstNameValue, setFirstNameValue] = useState('')
   const [firstNameInvalid, setFirstNameInvalid] = useState(false)
+  const [lastNameValue, setLastNameValue] = useState('')
   const [lastNameInvalid, setLastNameInvalid] = useState(false)
+  const [emailValue, setEmailValue] = useState('')
   const [emailInvalid, setEmailInvalid] = useState(false)
 
-  const validateInputs = (firstName, lastName, email) => {
+  const validateInputs = () => {
     let invalid = false
 
-    if (!firstName) {
+    if (!firstNameValue) {
       setFirstNameInvalid(true)
       invalid = true
     } else {
       setFirstNameInvalid(false)
     }
 
-    if (!lastName) {
+    if (!lastNameValue) {
       setLastNameInvalid(true)
       invalid = true
     } else {
       setLastNameInvalid(false)
     }
 
-    if (!email || emailRegEx.test(email) === false) {
+    if (!emailValue || emailRegEx.test(emailValue) === false) {
       setEmailInvalid(true)
       invalid = true
     } else {
@@ -39,60 +66,43 @@ const SignUpForm = () => {
   const handleSubmit = event => {
     event.preventDefault()
     event.stopPropagation()
-    const form = event.currentTarget
-    const firstNamed = form.elements.firstName.value.trim()
-    const lastName = form.elements.lastName.value.trim()
-    const email = form.elements.email.value.trim()
-    const isValid = validateInputs(firstNamed, lastName, email)
+    const isValid = validateInputs()
     if (isValid) {
       // TODO: html encode values and submit form
     }
   }
 
   return (
-    <Form noValidate onSubmit={handleSubmit}>
-      <h3>Sign Up to Our Newsletter</h3>
-      <Form.Group controlId='firstName'>
-        <Form.Label>First name</Form.Label>
-        <Form.Control
-          type='text'
-          placeholder='Enter first name'
-          isInvalid={firstNameInvalid}
-        />
-        <Form.Control.Feedback type='invalid'>
-          This is a required field
-        </Form.Control.Feedback>
-      </Form.Group>
-
-      <Form.Group controlId='lastName'>
-        <Form.Label>Last name</Form.Label>
-        <Form.Control
-          type='text'
-          placeholder='Enter last name'
-          isInvalid={lastNameInvalid}
-        />
-        <Form.Control.Feedback type='invalid'>
-          This is a required field
-        </Form.Control.Feedback>
-      </Form.Group>
-
-      <Form.Group controlId='email'>
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          as='input'
-          type='text'
-          placeholder='Enter email'
-          isInvalid={emailInvalid}
-        />
-        <Form.Control.Feedback type='invalid'>
-          Please type a valid email address
-        </Form.Control.Feedback>
-      </Form.Group>
-
-      <Button variant='primary' type='submit'>
+    <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <h3 className={classes.heading}>Sign Up to Our Newsletter</h3>
+      <TextField
+        error={firstNameInvalid}
+        helperText={firstNameInvalid ? 'This is a required field' : ''}
+        label="First name"
+        onChange={evt => setFirstNameValue(evt.target.value)}
+        type='text'
+        value={firstNameValue}
+      />
+      <TextField
+        error={lastNameInvalid}
+        helperText={lastNameInvalid ? 'This is a required field' : ''}
+        label="Last name"
+        onChange={evt => setLastNameValue(evt.target.value)}
+        type='text'
+        value={lastNameValue}
+      />
+      <TextField
+        error={emailInvalid}
+        helperText={emailInvalid ? 'Please type a valid email address' : ''}
+        label="Email"
+        onChange={evt => setEmailValue(evt.target.value)}
+        type='text'
+        value={emailValue}
+      />
+      <Button variant="contained" type="submit">
         Submit
       </Button>
-    </Form>
+    </form>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style/index.css'
 import './style/overrides.css'
 import App from './components/app'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default App
