d98068b50c47b2a7d3a684a398c70eee85f8bcf3
moicez <moicez@gmail.com>
2020-02-01 12:42:20 -0800

    Updates

---
+++ precache-manifest.5281f5d93ea882e95741af670ec5d965.esm.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa06671ec829f06387f7b53e16a82d79",
+    "url": "./assets/carousel/conference01.jpg"
+  },
+  {
+    "revision": "624334ca49c805ddaf760f0257d245e2",
+    "url": "./assets/carousel/conference02.jpg"
+  },
+  {
+    "revision": "4b9afc75a876039e6e04ba0f6c540ed3",
+    "url": "./assets/carousel/mushrooms01.jpg"
+  },
+  {
+    "revision": "e4d5f501f5db93a4c24a0d59090bd79b",
+    "url": "./assets/carousel/mushrooms02.jpg"
+  },
+  {
+    "revision": "560042adb68d371bff4e1a5870146522",
+    "url": "./assets/carousel/mushrooms03.jpg"
+  },
+  {
+    "revision": "c186856ff68bf195cce49ee73efe00dc",
+    "url": "./assets/carousel/mushrooms04.jpg"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "./assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "./assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "./assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "./assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "./assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "./assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b9a6340908d27f2c59d2",
+    "url": "./bundle.496ba.css"
+  },
+  {
+    "revision": "b059b9cd9b43ad36041d7274841a0e31",
+    "url": "./bundle.719eb.esm.js"
+  },
+  {
+    "revision": "fa4ceaadc563f50a31057a6aa3049eec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b50323e7293f266b5d553b8deb022f2",
+    "url": "./polyfills.5e8e9.esm.js"
+  },
+  {
+    "revision": "bc0e94ec035068b75db4",
+    "url": "./route-home/index.jsx.chunk.88129.css"
+  },
+  {
+    "revision": "5fe65c825c9be70b9bdf968748f3a419",
+    "url": "./route-home/index.jsx.chunk.c6c55.esm.js"
+  },
+  {
+    "revision": "5fefb0d75384b5306547",
+    "url": "./route-profile/index.jsx.chunk.4025b.css"
+  },
+  {
+    "revision": "842550694915c2bb32515183c7082650",
+    "url": "./route-profile/index.jsx.chunk.ade19.esm.js"
+  }
+]);
---
+++ precache-manifest.6ce2a2293bcdbaf5dad5fa85c3b62251.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa06671ec829f06387f7b53e16a82d79",
+    "url": "./assets/carousel/conference01.jpg"
+  },
+  {
+    "revision": "624334ca49c805ddaf760f0257d245e2",
+    "url": "./assets/carousel/conference02.jpg"
+  },
+  {
+    "revision": "4b9afc75a876039e6e04ba0f6c540ed3",
+    "url": "./assets/carousel/mushrooms01.jpg"
+  },
+  {
+    "revision": "e4d5f501f5db93a4c24a0d59090bd79b",
+    "url": "./assets/carousel/mushrooms02.jpg"
+  },
+  {
+    "revision": "560042adb68d371bff4e1a5870146522",
+    "url": "./assets/carousel/mushrooms03.jpg"
+  },
+  {
+    "revision": "c186856ff68bf195cce49ee73efe00dc",
+    "url": "./assets/carousel/mushrooms04.jpg"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "./assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "./assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "./assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "./assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "./assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "./assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b9a6340908d27f2c59d2",
+    "url": "./bundle.496ba.css"
+  },
+  {
+    "revision": "b9a6340908d27f2c59d2",
+    "url": "./bundle.b9a63.js"
+  },
+  {
+    "revision": "fa4ceaadc563f50a31057a6aa3049eec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7127dfa32f5d5a9dccf1",
+    "url": "./polyfills.7127d.js"
+  },
+  {
+    "revision": "bc0e94ec035068b75db4",
+    "url": "./route-home/index.jsx.chunk.88129.css"
+  },
+  {
+    "revision": "bc0e94ec035068b75db4",
+    "url": "./route-home/index.jsx.chunk.bc0e9.js"
+  },
+  {
+    "revision": "5fefb0d75384b5306547",
+    "url": "./route-profile/index.jsx.chunk.4025b.css"
+  },
+  {
+    "revision": "5fefb0d75384b5306547",
+    "url": "./route-profile/index.jsx.chunk.5fefb.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "./sw-debug.js"
+  },
+  {
+    "revision": "791ff76021b53a8963a102bcedae21cb",
+    "url": "./sw-esm.js"
+  },
+  {
+    "revision": "b5b1d18f306645d817de609c193bef4f",
+    "url": "./sw.js"
+  }
+]);
