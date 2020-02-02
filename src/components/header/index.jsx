7581f5bedbf1fc280e6dcb10d03a22f0388611a2
moicez <moicez@gmail.com>
2020-02-02 09:25:19 -0800

    fix: deployment to gh pages

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,20 @@
 import { h } from 'preact'
 import { Link } from 'preact-router/match'
 import style from './style.css'
+import App from '../app'
 
-const Header = () => (
+const Header = ({ path }) => (
   <header class={style.header}>
     <h1>Let'em Grow</h1>
     <nav>
-      <Link activeClassName={style.active} href='/'>
+      <Link activeClassName={style.active} href={path}>
         Home
       </Link>
-      <Link activeClassName={style.active} href='/profile'>
+      <Link activeClassName={style.active} href={`${path}profile`}>
         Me
       </Link>
-      <Link activeClassName={style.active} href='/profile/john'>
-        John
+      <Link activeClassName={style.active} href={`${path}profile/juan`}>
+        Juan
       </Link>
     </nav>
   </header>
