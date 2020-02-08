fd2fcc92a018a9746994a2cf42c5e1cae13fcc6c
moicez <moicez@gmail.com>
2020-02-07 22:24:24 -0800

    feat: connect and insert into mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb');
+
+/**
+ * Initialization code
+ */
 const app = express()
 const port = process.env.NODE_ENV === 'production' ? 80 : 3000
+let mongoInstance;
+const mongoUri = "mongodb+srv://naturepdxwebsite:password@cluster0-wzmm5.mongodb.net/test?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
+/**
+ * Middleware
+ */
 app.use(bodyParser.json())
 app.use(cors())
 
+/**
+ * Request Handlers
+ */
 const emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-app.post('/api/newsletter', cors(), (req, res) => {
+app.post('/api/newsletter', cors(), async (req, res) => {
   const { firstName: encodedFirstName, lastName: encodedLastName, email: encodedEmail } = req.body
   if (!encodedFirstName || !encodedLastName || !encodedEmail) {
     return res.sendStatus(400)
@@ -22,8 +36,25 @@ app.post('/api/newsletter', cors(), (req, res) => {
     return res.sendStatus(400)
   }
   console.log('success!', subscriber)
-  // todo add data to DB
+  try {
+    const collection = mongoInstance.collection('subscribers');
+    await collection.insetOne(subscriber)
+  } catch (err) {
+    return res.sendStatus(400)
+  }
   res.sendStatus(200)
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/**
+ * Server start
+ */
+client.connect(err => {
+  if (err) {
+    return console.log(err)
+  }
+  mongoInstance = mongoClient.db('Cluster0');
+  app.listen(port, () => {
+    console.log(`listening on ${port}`)
+  })
+})
