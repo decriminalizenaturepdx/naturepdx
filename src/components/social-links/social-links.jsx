a0404f6823530f1569723c008b4794e2bb75b539
moicez <moicez@gmail.com>
2020-02-23 13:45:05 -0800

    chore: switch to react because preact is not prod ready

--- src/components/social-links/social-links.jsx
+++ src/components/social-links/social-links.jsx
@@ -1,33 +1,36 @@
-import { h } from 'preact'
-import styles from './styles.css'
-import facebook from '../../../assets/imgs/facebook-64.png'
-import instagram from '../../../assets/imgs/instagram-64.png'
-import twitter from '../../../assets/imgs/twitter-64.png'
+import React  from 'react';
+import './styles.css'
+import facebook from '../../assets/imgs/facebook-64.png'
+import instagram from '../../assets/imgs/instagram-64.png'
+import twitter from '../../assets/imgs/twitter-64.png'
 
-const Header = () => (
-  <div class={styles.root}>
+const SocialLinks = () => (
+  <div className="SocialLinks">
     <a
-      class={styles.links}
-      href='https://www.facebook.com/Decriminalize-Nature-Portland-1826297640855055/'
-      target='_blank'
+      class="SocialLink"
+      href="https://www.facebook.com/Decriminalize-Nature-Portland-1826297640855055/"
+      target="_blank"
+      rel="noopener noreferrer"
     >
       <img src={facebook} alt='Facebook page' />
     </a>
     <a
-      class={styles.links}
-      href='https://www.instagram.com/decriminalizenatureportland/'
-      target='_blank'
+      class="SocialLink"
+      href="https://www.instagram.com/decriminalizenatureportland/"
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      <img src={instagram} alt='Instagram feed' />
+      <img src={instagram} alt="Instagram feed" />
     </a>
     <a
-      class={styles.links}
+      class="SocialLink"
       href='https://twitter.com/NaturePortland'
-      target='_blank'
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      <img src={twitter} alt='Twitter feed' />
+      <img src={twitter} alt="Twitter feed" />
     </a>
   </div>
 )
 
-export default Header
+export default SocialLinks
