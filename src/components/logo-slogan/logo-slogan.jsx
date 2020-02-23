d2bd1b4c8f03ed69555a49499a9564a5e6d2c358
moicez <moicez@gmail.com>
2020-02-23 14:09:14 -0800

    chore: optimization

--- src/components/logo-slogan/logo-slogan.jsx
+++ src/components/logo-slogan/logo-slogan.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Logo from '../../assets/imgs/logo.jpg'
 import './styles.css'
 
 const LogoSlogan = () => {
   return (
     <div className="logo-slogan">
-      <img src={Logo} alt="Logo" />
+      <img src="https://res.cloudinary.com/dakhemwii/image/upload/v1582421214/logo_k2k2yy.jpg" alt="Logo" />
       <p>
         <h2>
           - It is an unalienable right to develop your own relationship with Nature
